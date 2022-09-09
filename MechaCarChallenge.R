724fc31faab6ea5a6ad9e2bc5a409323ea50ddf9
Gladys Robles <105124485+lalylaly1227@users.noreply.github.com>
2022-09-09 14:58:36 -0400

    update

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,3 +1,4 @@
+## Deliverable 1 - Gladys Robles
 # 3. Use the library() function to load the dplyr package
 library(dplyr)
 
@@ -11,3 +12,4 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 
 #6. Using the summary() function, determine the p-value and the r-squared value for the linear regression model.
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mecha_mpg)) 
+
